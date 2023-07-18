b69c8993753605a05b15bf778da441978ea264cf
SamuelCoulomb <samuel_lucbomo@hotmail.fr>
2023-07-18 23:02:07 +0200

    Mod

--- cours-bitcoin/btc.js
+++ cours-bitcoin/btc.js
@@ -1,2 +1,30 @@
 #!/usr/bin/env node
-console.log("Hello world")
+
+const axios = require('axios');
+
+async function main() {
+
+    try {
+
+        let currency = 'USD';
+        if (process.argv[2]) {
+            currency = process.argv[2].toUpperCase();
+        } else {
+            currency = 'USD';
+        }
+
+        const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json');
+
+        if (!response.data.bpi[currency]) {
+            throw new Error('Devise inconnue');
+        }
+        const updateAt = response.data.time.updated;
+        const rate = response.data.bpi[currency].rate;
+        console.log(`> 1 BTC = ${rate} ${currency} (${updateAt})`);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();

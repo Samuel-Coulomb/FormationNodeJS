609a1b3e81efdbd9ed045fa77caa5bdd00d2380b
Samuel-Coulomb <samuel_lucbomo@hotmail.fr>
2023-07-18 19:50:17 +0200

    Mod

--- Introduction/async.js
+++ Introduction/async.js
@@ -3,9 +3,32 @@ function hello() {
 }
 setTimeout(hello, 2000);
 
+function attendre2secondes() {
+
+    return new Promise((resolve) => {
+
+        setTimeout(() => {
+            console.log("Timer temriné");
+            resolve("Résolu");
+        }, 2000);
+    });
+}
 
 
 
 setTimeout(() => {
-    console.log("Hello 2")
+    console.log("Hello 2");
 }, 3000);
+
+console.log("3 Terminé")
+attendre2secondes().then(() => {
+    console.log("3. Timer terminé")
+})
+
+async function main() {
+    console.log("111");
+    console.log(await attendre2secondes());
+    console.log("222");
+}
+
+main();
